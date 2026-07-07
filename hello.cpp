1596cd6edda73aa5ddffb4c86f278695d47341f3
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2026-07-07 17:52:37 -0300

    1-commit

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    std:: cout << "Hello W" << std::endl;
+    return 0;
+}
+
+//g++ hello.cpp -o hello.exe
+//.\hello.exe
+
+/*
+teste
+teste2
+*/
---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Setup Complete!" << endl; //prints text to the console
+    return 0;
+}
+
+//g++ test.cpp -o test.exe
+//.\test.exe
