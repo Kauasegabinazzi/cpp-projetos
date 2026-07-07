cbf55a608479ed7833a01baec51a79ec1137b026
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2026-07-07 18:35:30 -0300

    2 commit

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
 
-int main(){
-    std:: cout << "Hello W" << std::endl;
+int main()
+{
+    std::cout << "================================" << std::endl;
+    std::cout << "      PERSONAL INTRODUCTION     " << std::endl;
+    std::cout << "================================" << std::endl;
+
+    std::cout << "Name : Kaua Segabinazzi" << std::endl;
+    std::cout << "Role : Full stack developer" << std::endl;
+    std::cout << "Programming languages : C++, C#" << std::endl;
+    std::cout << "Hobbies : Going to the gym" << std::endl;
+    std::cout << "================================" << std::endl;
+
+    std::cout << "\nEducation" << std::endl;
+    std::cout << "--------------------------------" << std::endl;
+    std::cout << "Degree : Computer Science" << std::endl;
+    std::cout << "School : La Salle University" << std::endl;
+    std::cout << "Year : 2026" << std::endl;
+
+    std::cout << "\nCAREER GOALS" << std::endl;
+    std::cout << "--------------------------------" << std::endl;
+    std::cout << "Become a successful gamer developer" << std::endl;  
+
+    std::cout << "\nTOPICS I'M EXCITED ABOUT" << std::endl;
+    std::cout << "--------------------------------" << std::endl;
+    std::cout << "Games" << std::endl;
+
+    std::cout << "\nThank you for taking the time to read my personal introduction!" << std::endl;
+
     return 0;
 }
 
-//g++ hello.cpp -o hello.exe
+// g++ hello.cpp -o hello.exe
 //.\hello.exe
 
 /*
---
+++ personal_intro.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    std:: cout << "Full stack developer" << std::endl;
+
+    return 0;
+}
+
+/*
+teste
+teste
+*/
